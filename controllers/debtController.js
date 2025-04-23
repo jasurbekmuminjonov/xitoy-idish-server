5ff31829388186cc68ae8ae1fa7239b5695d297d
Jasur <j31548359@gmail.com>
2025-04-23 19:56:05 +0500

    push

--- controllers/debtController.js
+++ controllers/debtController.js
@@ -4,10 +4,10 @@ const Sale = require("../models/Sale");
 const Product = require("../models/Product");
 
 const createDebt = async (req, res) => {
-  const { clientId, productId, quantity, currency, totalAmount, paymentMethod, unit, sellingPrice, warehouseId, discount, dueDate } =
+  const { clientId, productId, quantity, currency, totalAmount, paymentMethod, paymentHistory, unit, sellingPrice, warehouseId, discount, dueDate } =
     req.body;
 
-  if (!clientId || !productId || !quantity || !totalAmount || !warehouseId) {
+  if (!clientId || !productId || !quantity || !totalAmount || !warehouseId || !paymentHistory) {
     return res.status(400).json({ message: "All fields are required." });
   }
 
@@ -41,6 +41,7 @@ const createDebt = async (req, res) => {
       unit,
       sellingPrice,
       warehouseId,
+      paymentHistory,
       totalAmount,
       currency,
       discount,
