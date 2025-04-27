40d55cd2442494f596d574ee43cae33dc1129e1f
bekicom <adelkauz6@gmail.com>
2025-04-27 13:04:21 +0500

    rasxod usd

--- models/Expense.js
+++ models/Expense.js
@@ -6,6 +6,15 @@ const expenseSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+    amountInUZS: {
+      type: Number,
+      required: true,
+    },
+    currency: {
+      type: String,
+      enum: ["UZS", "USD"],
+      required: true,
+    },
     date: {
       type: Date,
       required: true,
